95b0c48c575e1fdc46038784a6e92cdb6ff55e58
kifahnaseer <kifahnaeer28@gmail.com>
2023-03-02 15:59:21 +0300

    generated users

--- db/migrate/20230302121817_create_tasks.rb
+++ db/migrate/20230302121817_create_tasks.rb
@@ -1,10 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
+
   def change
     create_table :tasks do |t|
       t.string :name
       t.text :description
       t.date :date_to_complete
       t.boolean :follow_up
+      t.integer :id_of_user
+      
     end
   end
 end
