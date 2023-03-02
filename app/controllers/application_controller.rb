8fd83d691b8ecfa08d91c456dc78e47c6a40fd7e
kifahnaseer <kifahnaeer28@gmail.com>
2023-03-02 13:15:30 +0300

    added application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/tasks" do
+    {tasks =Task.all }
+    tasks.to_json
+  end
+
+  post '/tasks' do
+    tasks = Task.create(
+      task_name: params[:task_name],
+      description: params[:description],
+      due_date: params[:due_date],
+      status: params[:status => "Complete"] || false
+    )
+    tasks.to_json
+  end
+
+  patch '/tasks/:id' do
+    tasks = Task.find(params[:id])
+    tasks.update(
+      description: params[:description],
+      due_date: params[:due_date]
+    )
+    tasks.to_json
+  end
+
+  delete '/tasks/:id' do
+    tasks = Task.find(params[:task_name])
+    tasks.destroy
+    tasks.to_json
+  end
+
+  get '/login' do
+    erb :login
   end
 
 end
