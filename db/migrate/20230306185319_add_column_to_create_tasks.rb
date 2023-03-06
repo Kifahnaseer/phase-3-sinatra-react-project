e3dc64829626c96aafff71e591b355c3976aa187
kifahnaseer <kifahnaeer28@gmail.com>
2023-03-06 21:58:28 +0300

    added a column in tasks table

---
+++ db/migrate/20230306185319_add_column_to_create_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnToCreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :date, :date
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_125827) do
+ActiveRecord::Schema.define(version: 2023_03_06_185319) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_03_02_125827) do
     t.date "date_to_complete"
     t.boolean "follow_up"
     t.integer "id_of_user"
+    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
